1e544b8c1c3234726d20a573ad7e6cb78b129045
Avinash Malhotra <avimalhotra505@gmail.com>
2022-02-26 10:56:43 +0530

    Node JS OS and FS Modules

--- src/app.js
+++ src/app.js
@@ -2,18 +2,34 @@ const dotenv=require("dotenv").config();
 const colors=require('colors');
 const mod=require("./module");
 const maths=require("./maths");
-
+const os=require('os');
+require('./fs');
 
 //console.log(`Running node js app on ${process.env.PORT}`);
 
-console.log(process.version.green);
+//console.log(process.version.green);
 
 //console.log(__filename);
 //console.log(__dirname);
 
 //console.log(mod);
-console.log(global.name.red);
+//console.log(global.name.red);
 
 //console.log(`Area of circle with radius ${maths.r} is ${maths.area}.`);
 
-console.log(maths.toFixed(2));
+//console.log(maths.toFixed(2));
+
+//console.log(os.cpus().length);                         // 8 threads
+//console.table(os.cpus());
+
+//console.log(os.freemem()/1024/1024);
+//console.log(os.totalmem()/1024/1024);
+
+
+
+//console.log(os.networkInterfaces());
+//console.log(os.platform());
+//console.log(os.type());
+//console.log(os.uptime()/3600);
+
+//console.log(os.userInfo());
---
+++ src/fs.js
@@ -0,0 +1,31 @@
+const fs=require("fs");
+
+//let file=fs.readFileSync('src/data.txt',{encoding:'utf-8'});
+//console.log(file);
+
+/* fs.readFile('src/data.txt',{encoding:'utf-8'},(err,data)=>{
+    if(err){ console.error(err);}
+    else{console.log(data);}
+}); */
+/* fs.stat('src/step.txt',{encoding:'utf-8'},(err,stats)=>{
+     if(err){ console.error(err);}
+     else{console.log(stats.size);}
+ }); */
+
+let str="lorem ipsum dolor\n";
+
+/*  fs.appendFile("src/data.txt",str,'utf-8',(err,data)=>{
+     if(err){ console.error(err);}
+     else{console.log("written in file");}
+ }); */
+
+ fs.unlink("src/del.txt",(err)=>{
+      if(err){
+          console.error(err);
+      }
+      else{
+          console.log("file deleted");
+      }
+ })
+
+console.log("done");
