65ef5234ca4dcb1fff459f6da8c5e34f36a2b2e5
Avinash Malhotra <avimalhotra505@gmail.com>
2022-03-20 11:10:19 +0530

    Express Session and REST API

--- src/app.js
+++ src/app.js
@@ -5,6 +5,15 @@ let app=express();
 const path=require('path');
 const cookieparser=require('cookie-parser');
 const bodyParser = require('body-parser');
+const session=require('express-session');
+const parseurl=require('parseurl');
+app.set('trust proxy', 1); 
+app.use(session({
+     secret:"session",
+     resave:false,
+     saveUninitialized:true,
+     cookie:{secure:false,maxAge:5000}                          // session expires after 1 min
+ }));
 
 
 app.use(cookieparser());
@@ -13,7 +22,7 @@ app.use(bodyParser.json());
 
 require('dotenv').config();
 
-//app.use(express.static(path.resolve('src/public')));
+app.use(express.static(path.resolve('src/public')));
 
 /* express().use((req,res)=>{
      res.end("express app");
@@ -24,9 +33,20 @@ require('dotenv').config();
      res.status(200).send("<h1>express app running</h1>");
 }); */
 
-app.use((req,res,next)=>{
+/* app.use((req,res,next)=>{
      //console.log(`Login at ${Date.now()}`);
      next();
+}); */
+
+app.use((req,res,next)=>{
+     if(!req.session.views){req.session.views={};}
+
+     var pathname=parseurl(req).pathname;
+
+     req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+
+     next()
+
 });
 
 app.get("/",(req,res)=>{
@@ -36,13 +56,34 @@ app.get("/",(req,res)=>{
      //res.status(200).send(req.query);
      //res.status(200).send(Object.keys(req.cookies));
 
-    if(req.cookies.location){
+    /* if(req.cookies.location){
           res.status(200).send(req.cookies);
     }
     else{
           res.status(200).send("no cookies");
-    }
+    } */
+    //req.session.time=Date.now();
+    //res.status(200).send(`Session ID is ${req.sessionID} and time is ${req.session.time}`);
+    res.send( req.sessionID +', Session Views :  '+ req.session.views['/'] + ' times');
 });
+
+let cars=[
+          {"name": "swift","type": "hatchback","price":800000},
+          {"name": "ciaz","type": "sedan","price":1000000},
+          {"name": "baleno","type": "hatchback","price":850000},
+          {"name": "scross","type": "crossover","price":11500000},
+          {"name": "brezza","type": "suv","price":990000},
+          {"name": "gypsy","type": "suv","price":750000}
+     ];
+
+app.get("/api",(req,res)=>{
+
+     res.header('Access-Control-Allow-Origin',"*");
+     let days=["sun","mon","tues","wed","thurs","fri","sat"];
+     return res.status(200).send(cars);
+
+});
+
 app.get("/savecookie",(req,res)=>{
      res.cookie("name","avinash", {maxAge:86400, httpOnly: true});
      res.send("cookie saved");
@@ -86,6 +127,7 @@ app.post('/postdata',(req,res)=>{
 /* from router */
 const admin=require('./routes/admin');
 const user=require('./routes/users');
+const { off } = require('process');
 app.use("/admin",admin);
 app.use("/user",user);
 
