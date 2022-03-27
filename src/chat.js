749d07723c348223da56603cb50bf0fb3bd2f78a
Avinash Malhotra <avimalhotra505@gmail.com>
2022-03-27 11:30:34 +0530

    Nodemailer and Socket.io

--- src/chat.js
+++ src/chat.js
@@ -1,16 +1,29 @@
-//import { Server } from "socket.io";
-//const Server=require("socket.io")
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/chat.html');
+});
 
-//const io = new Server(3000);
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-const io = require('socket.io')();
-io.on("connection", (socket) => {
-  // send a message to the client
-  socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
-
-  // receive a message from the client
-  socket.on("hello from client", (...args) => {
-    // ...
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
